841673afbcc984a5b08e9e8fa061f6694b6ba283
danijelkelava <dykyprod@gmail.com>
2017-12-22 16:29:06 +0100

    commands update

--- index.php
+++ index.php
@@ -10,35 +10,27 @@ $robot = new Robot();
 
 $handle = fopen("test/commands.txt", "r");
 if ($handle) {
-    while (($line = fgets($handle)) !== false) {
+    while (($line = fgets($handle)) !== false && strlen($line) > 0) {
+        //var_dump($line);
         $lineSpaceBreak = explode(" ", $line);
         //var_dump($lineSpaceBreak);
-        if(count($lineSpaceBreak) == 1){
-               
-            if(trim($lineSpaceBreak[0]) == "MOVE"){
+        $params = explode(",", $lineSpaceBreak[1]);
+
+        foreach ($lineSpaceBreak as $key => $value){
+            if (trim($lineSpaceBreak[$key]) == "PLACE") {
+                $robot->place(trim($params[0]), trim($params[1]), trim($params[2]));
+            }elseif (trim($lineSpaceBreak[$key]) == "MOVE") {
                 $robot->move();
-            }
-            elseif(trim($lineSpaceBreak[0]) == "LEFT"){
+            }elseif (trim($lineSpaceBreak[$key]) == "LEFT"){
                 $robot->left();
-                
-            }
-            elseif(trim($lineSpaceBreak[0]) == "RIGHT"){
+            }elseif(trim($lineSpaceBreak[$key]) == "RIGHT"){
                 $robot->right();
-                
-            }
-            elseif(trim($lineSpaceBreak[0]) == "REPORT"){
-            	echo "<pre>";
+            }elseif(trim($lineSpaceBreak[$key]) == "REPORT"){
+                echo "<pre>";
                 print_r($robot->report());
                 echo "</pre>";
             }
-        }
-        else if(count($lineSpaceBreak) > 1){
-            if($lineSpaceBreak[0] == "PLACE"){
-                $params = explode(",", $lineSpaceBreak[1]);
-                //var_dump($params);
-                $robot->place(trim($params[0]), trim($params[1]), trim($params[2]));
-            }
-        }
+        }        
     }
 } else {
     echo "Can't open file!";
